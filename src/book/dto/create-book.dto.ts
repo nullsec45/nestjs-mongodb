5e27ee39e1e1c273b2f8b3cfd08fc7a656b4a650
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-11-06 13:16:20 +0700

    feat(book) : add validation in create & update dto.

--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,10 +1,37 @@
 import {Category} from '../schemas/book.schema';    
+import { User } from 'src/auth/schemas/user.schema';
 
+import {
+    IsNotEmpty,
+    IsString,
+    MinLength,
+    IsEmail,
+    IsNumber,
+    IsEnum,
+    IsEmpty
+} from 'class-validator';
 
 export class CreateBookDto{
+    @IsNotEmpty()
+    @IsString()
     readonly title:string;
+    
+    @IsNotEmpty()
+    @IsString()
     readonly description:string
+    
+    @IsNotEmpty()
+    @IsString()
     readonly author:string;
+    
+    @IsNotEmpty()
+    @IsNumber()
     readonly price:number;
-    readonly category:string;
+    
+    @IsNotEmpty()
+    @IsEnum(Category,{message:'Please enter correct category.'})
+    readonly category:Category;
+
+    @IsEmpty({message:'You cannot pass user id'})
+    readonly user:User;
 }
--- src/book/dto/update-book.dto.ts
+++ src/book/dto/update-book.dto.ts
@@ -1,10 +1,35 @@
 import {Category} from '../schemas/book.schema';    
+import { User } from 'src/auth/schemas/user.schema';
 
+import {
+    IsNotEmpty,
+    IsString,
+    IsEmpty,
+    IsNumber,
+    IsEnum
+} from 'class-validator';
 
 export class UpdateBookDto{
+    @IsNotEmpty()
+    @IsString()
     readonly title:string;
+    
+    @IsNotEmpty()
+    @IsString()
     readonly description:string
+    
+    @IsNotEmpty()
+    @IsString()
     readonly author:string;
+    
+    @IsNotEmpty()
+    @IsNumber()
     readonly price:number;
-    readonly category:string;
+    
+    @IsNotEmpty()
+    @IsEnum(Category,{message:'Please enter correct category.'})
+    readonly category:Category;
+
+    @IsEmpty({message:'You cannot pass user id'})
+    readonly user:User;
 }
